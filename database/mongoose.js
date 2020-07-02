d5f625bd9e02076eb696f9926c8ee9d9123889fd
hellodit <asditap@gmail.com>
2020-07-02 14:27:12 +0700

    test database running

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const db = mongoose.connection; 
 
-mongoose.connect(process.env.MONGODB_HOST,{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_HOST,{useNewUrlParser: true});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('database connected!');
 });
