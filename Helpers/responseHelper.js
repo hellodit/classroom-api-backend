8aac4e59f613dbd68adaf4b611c00b4955d02830
hellodit <asditap@gmail.com>
2020-07-09 14:47:30 +0700

    add-reponse-helper

---
+++ Helpers/responseHelper.js
@@ -0,0 +1,49 @@
+responseData = function (data){
+    return {
+        'result': true, 
+        data,
+    }
+}
+
+
+responseDataCount = function(data){
+    return {
+        'result':true, 
+        'count': data.length, 
+        data
+    }
+}
+
+responseInfo = function(info, result = null){
+    if(result === null){
+        return{
+            'result': false, 
+            'data' : [],
+            info
+        }
+    }else{
+        return{ 
+            'result': true, 
+            data,
+            info
+        }    
+    }
+}
+
+responseBadRequest = function(message, info){
+    if(info ==""){
+        return{
+            message
+        }
+    }else{
+        return{
+            message,
+            info
+        }
+    }
+}
+
+module.exports = {
+    responseData,
+    responseDataCount
+}
