9c4ce8166c32918d79dee7db289e1bcd2f8cce4e
hellodit <asditap@gmail.com>
2020-07-09 15:02:19 +0700

    refactor-material-response

--- controllers/MaterialController.js
+++ controllers/MaterialController.js
@@ -1,4 +1,5 @@
 const Room = require('../models/room');
+const {responseData,responseDataCount } = require('../Helpers/responseHelper'); 
 
 const index = async(req,res) => {
     const _id = req.params.roomid;    
@@ -11,10 +12,7 @@ const index = async(req,res) => {
         if (!room) {
             return res.status(400).send(); 
         }        
-       res.status(201).send({
-           'status': true, 
-           'data':  room.materials 
-        });
+       res.status(201).send(responseDataCount(room.materials));
     } catch (error) {
         res.send({'status':false, 'data' : []}).status(500)
     }
@@ -33,11 +31,7 @@ const craete = async(req, res) => {
         room.materials.push(req.body); 
         await room.save();
 
-        res.status(201).send({ 
-            'status' : true,
-            'message': 'Data successfuly added!',
-            'data': room
-        });
+        res.status(201).send(responseInfo('Data successfuly added!', room));
 
     } catch (error) {
         res.status(500).send(error)
